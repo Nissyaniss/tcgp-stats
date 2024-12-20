1b3e75df0b58934a322821f7f66a4bcc7d2ae5ff
Maxime <maximechagnon00@gmail.com>
2024-12-20 20:57:14 +0100

    feat: collection for genetic apex set

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::read_to_string;
+use std::io::prelude::*;
 
 use comfy_table::Table;
 use clap::Parser;
@@ -8,6 +9,9 @@ struct Args {
     /// Display the current drop rate table of the game
     #[clap(short, long)]
     drop_rate: bool,
+
+    #[clap(short, long, value_delimiter = ' ', num_args = 1..)]
+    cards: Option<Vec<String>>,
 }
 
 fn main() {
@@ -15,6 +19,29 @@ fn main() {
 
     if args.drop_rate {
         display_drop_rate();
+    }
+
+    if let Some(cards) = args.cards {
+        let data = read_csv("./src/csv/genetic_apex.csv");
+        let mut data: Vec<Vec<String>> = data.lines().map(|line| line.split(',').map(|s| s.to_string()).collect()).collect::<Vec<Vec<String>>>();
+
+        for card in cards {
+            if card == "all" {
+                for line in data.clone() {
+                    data[line[1].split_whitespace().last().unwrap().parse::<usize>().unwrap() - 1][0] = "TRUE".to_string();
+                }
+            } else {
+                for line in data.clone() {
+                    if line[2] == card {
+                        data[line[1].split_whitespace().last().unwrap().parse::<usize>().unwrap() - 1][0] = "TRUE".to_string();
+                        break;
+                    }
+                }
+            }
+        }
+
+        let mut file = std::fs::File::create("./src/csv/genetic_apex.csv").unwrap();
+        file.write_all(data.iter().map(|line| line.join(",") + "\n").collect::<String>().as_bytes()).unwrap();
     }
 }
 
